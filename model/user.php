ae74a61098c1b6451e1d2febf9bf2618f1e66986
misaghsoltani <misaghsoltani@users.noreply.github.com>
2020-12-24 16:18:25 +0330

    making a class for user in '/model' directory with a class named person and another named user, which extends the person class and adds some attributes like username, password and status

---
+++ model/user.php
@@ -0,0 +1,90 @@
+<?php
+
+abstract class person
+{
+    public $name;
+    public $family;
+
+    function getName() {
+        return $this->name;
+    }
+
+    function setName($name) {
+        $this->name = $name;
+    }
+
+    function getFamily() {
+        return $this->family;
+    }
+
+    function setFamily($family) {
+        $this->family = $family;
+    }
+}
+
+class user extends person
+{
+    private $username;
+    private $password;
+    private $status;
+
+    function getUsername() {
+        return $this->username;
+    }
+
+    public function setUsername($username) {
+        $this->username = $username;
+    }
+
+    function getPassword() {
+        return $this->password;
+    }
+
+    function setPassword($password) {
+        $this->password = md5($password);
+    }
+
+    function getStatus() {
+        return $this->status;
+    }
+
+    function setStatus($status) {
+        $this->$status = $status;
+    }
+
+/*    function checkUserPass() {
+        $paramTypes = "ss";
+        $Parameters = array($this->username, $this->password);
+        $result = database::ExecuteQuery('CheckUserPass', $paramTypes, $Parameters);
+
+        if(mysqli_num_rows($result) > 0)
+        {
+            $row = $result->fetch_array();
+            $this->setName($row["name"]);
+            $this->setFamily($row["family"]);
+            return true;
+        }
+        return false;
+    }
+
+    private function IsUsernameExist() {
+        $paramTypes = "s";
+        $Parameters = array($this->username);
+        $result = database::ExecuteQuery('IsUsernameExist', $paramTypes, $Parameters);
+
+        if(mysqli_num_rows($result) > 0)
+            return true;
+        return false;
+    }
+
+    function Save() {
+        if(!$this->IsUsernameExist()) {
+            $paramTypes = "ssss";
+            $Parameters = array($this->username, $this->password,
+                $this->name, $this->family);
+            database::ExecuteQuery('AddUser', $paramTypes, $Parameters);
+            return true;
+        }
+        return false;
+    }*/
+}
