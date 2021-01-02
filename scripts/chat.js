af3ed1a727e56e87768ce3c178118e8485514e2f
misaghsoltani <misaghsoltani@users.noreply.github.com>
2021-01-03 02:21:13 +0330

    Added update profile elements and scripts.

---
+++ scripts/chat.js
@@ -0,0 +1,7 @@
+//some code to show the modal popup for profile edit
+var modalBtn = document.querySelector('.left-top');
+var modalBg =document.querySelector('.modal-bg');
+
+modalBtn.addEventListener('click', function (){
+   modalBg.classList.add('active');
+});
