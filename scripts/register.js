d98edb3c3e9e6f80abd2ae4a13e8b3d9ebe09640
kaBuki44 <javadasghari13@gmail.com>
2021-01-03 02:34:21 +0330

    Added OnKeyPress actions to register script.

---
+++ scripts/register.js
@@ -0,0 +1,27 @@
+function isCharNumber(event){
+    if (event.keyCode < 48 || event.keyCode > 57)
+        return false;
+    return true;
+}
+function isTextEnglish(text) {
+    return !( new RegExp("[^\sa-zA-Z]").test(text));
+}
+function isCharEnglish(event){
+    if(isTextEnglish(String.fromCharCode(event.keyCode)))
+        return true;
+    return false;
+}
+
+function isCharValidFirstName(event){
+    return isCharEnglish(event);
+}
+function isCharValidLastName(event){
+    return isCharEnglish(event);
+}
+function isCharValidUsername(event){
+    return (isCharEnglish(event) || isCharNumber(event) || event.keyCode == 189);
+}
+function isCharValidPassword(event){
+    let pattern = /[a-zA-Z0-9=*$#!+-]/;
+    return (isCharEnglish(event) || isCharNumber(event) || pattern.test(String.fromCharCode(event.keyCode)));
+}
