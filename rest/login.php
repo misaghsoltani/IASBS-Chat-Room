d8a3b7db4ee8beeab5d8c88e58d7ad0e23096774
erfan <er.moharramkhani@gmail.com>
2021-01-03 23:38:36 +0330

    login implementation in rest api

--- rest/login.php
+++ rest/login.php
@@ -1,11 +1,25 @@
 <?php
+require_once 'core.php';
 
-
-
-$jwt = new JWT('secret');
 if ($_SERVER['REQUEST_METHOD'] != 'POST'){
-   // TODO : request method must be post
-}else{
+    Response::method_not_allowed_response();
+}
+
+try{
     $data = json_decode(file_get_contents('php://input'), true);
-    print_r($data);
+    if(json_last_error() !== JSON_ERROR_NONE){
+        Response::special_response(400, "Please check request format again.");
+    }
+    // check if all parameters are sent (or received at server )
+    $required_keys = ['username' , 'password'];
+    if (!count(array_diff($required_keys, array_keys($data))) == 0) {
+        // one of the values is not sent: code 422
+        Response::all_parameters_required_response();
+    }
+
+    $db = Database::get_instance();
+    
+    
+}catch (Throwable $exception){
+    Response::internal_error();
 }
