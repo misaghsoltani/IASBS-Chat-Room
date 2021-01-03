fc8715fbb7c52051d51ee8f16f52805526fd19ca
erfan <er.moharramkhani@gmail.com>
2021-01-03 03:53:25 +0330

    register service

--- rest/register.php
+++ rest/register.php
@@ -4,43 +4,59 @@ use Ahc\Jwt\JWT;
 
 require_once 'core.php';
 
-$data = json_decode(file_get_contents('php://input'), true);
-
-$db = new Response(Response::$UNAUTHORIZED);
 header('Content-Type: application/json');
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     try{
-        $db = Database::get_instance();
+        $data = json_decode(file_get_contents('php://input'), true);
+        if(json_last_error() !== JSON_ERROR_NONE){
+            Response::special_response(400, "Please check request format again.");
+        }
+        // check if all parameters are sent (or received at server )
+        $required_keys = ['username' , 'password','first_name', 'last_name'];
+        if (!count(array_diff($required_keys, array_keys($data))) == 0) {
+            // one of the values is not sent: code 422
+            Response::all_parameters_required_response();
+        }
 
+
+        $db = Database::get_instance();
+        if(!$db->ping()){
+            Response::special_response(500, "Internal server error.");
+        }
         // check if username exist:
         $username = $data['username'];
         $query = "CALL GETPROFILE('$username')";
 
         $result = $db->query($query);
         if($result->num_rows == 0){
+            $db->close();
+            $db = Database::get_instance();
             // there is no user with this username and we can register
-            $required_keys = ['username' , 'password',];
-            if (count(array_diff($required_keys, array_keys($data))) == 0) {
-                echo 'all exist';
-            }
             $password = $data['password'];
-            $first_name = $data['firs_name'];
+            $first_name = $data['first_name'];
             $last_name = $data['last_name'];
+
+            if (!$db->query("CALL REGISTER('$username', '$password', '$first_name', '$last_name')")) {
+                echo("Error description: " . $db -> error);
+            }
+
+
+            $db->commit();
+            $db->close();
+            Response::special_response(201, "User created.");
 
         }else{
             // username already exist
-            http_response_code(409);
-            echo ('
-        {"code": 409, "msg": "Username already taken."}
-        ');
+            Response::special_response(409, "Username already taken.");
         }
-    }catch (Exception $exception){
+    }catch (Throwable $exception){
 
+        Response::special_response(500, "Internal server error.");
     }
 }else{
     // TODO : return 405 code
     http_response_code(405);
 
-    echo ("405 codeee");
+    echo ("405 code");
 }
