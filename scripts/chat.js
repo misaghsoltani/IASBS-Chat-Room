f7ea2724a30bd2a1856e3ded6a356aee313f63ab
misaghsoltani <misaghsoltani@users.noreply.github.com>
2021-01-03 22:36:48 +0330

    Added context menu front codes.

--- scripts/chat.js
+++ scripts/chat.js
@@ -10,6 +10,36 @@ function closeEditProfile(){
    modalBg.classList.remove('active');
 }
 
-//code fro scrolling chat box to down
+//code for scrolling chat box to down
 var element = document.getElementsByClassName("right-middle");
 element[0].scrollTop = element[0].scrollHeight;
+
+//some code to show and hide edit menu arrow
+function showEditArrow(el){
+   let msg_br = el.querySelector('.msg-br');
+   let msg_menu = el.querySelector('.msg-menu-selector');
+   msg_br.style.display = "block";
+   msg_menu.style.display = "block";
+   if (el.classList.contains('me')) {
+      msg_menu.style.float = "left";
+   }
+   else {
+      msg_menu.style.float = "right";
+   }
+}
+
+function hideEditArrow(el){
+   let msg_br = el.querySelector('.msg-br');
+   let msg_menu = el.querySelector('.msg-menu-selector');
+   msg_br.style.display = "none";
+   msg_menu.style.display = "none";
+}
+
+function showContextMenu(){
+   let contextMenu = document.querySelector('.context-menu');
+   contextMenu.style.display = 'block';
+   contextMenu.style.left = event.clientX + 'px';
+   //alert(event.clientY);
+   contextMenu.style.top = event.clientY + 'px';
+   /*el.style.backgroundColor = 'black';*/
+}
