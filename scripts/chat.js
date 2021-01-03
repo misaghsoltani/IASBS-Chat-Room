a1a2908d6c6125b207ea79424ad9fbfb6c3ebdfd
kaBuki44 <javadasghari13@gmail.com>
2021-01-04 01:39:10 +0330

    Updated send and edit message functions.

--- scripts/chat.js
+++ scripts/chat.js
@@ -144,5 +144,16 @@ function editMessage(){
 }
 
 function sendMessage(){
-   alert(document.getElementById("msg-input").value);
+   if(checkMessageText(document.getElementById("msg-input").value)){
+      if(editingStatus == true){
+         //call php edit message with message id
+         currentMessageObject.childNodes[2].innerHTML = document.getElementById("msg-input").value;
+         document.getElementById("msg-input").value = "";
+      }
+      else{
+         //call php send message with message id
+         alert(document.getElementById("msg-input").value);
+         document.getElementById("msg-input").value = "";
+      }
+   }
 }
